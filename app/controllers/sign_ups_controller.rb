e30d392083b5c6fa7977b8ea9961ba27a14617c2
sarahcapecci <shlcapecci@gmail.com>
2016-02-11 09:04:03 -0500

    fixed validation when new login is created

--- app/controllers/sign_ups_controller.rb
+++ app/controllers/sign_ups_controller.rb
@@ -1,18 +1,22 @@
 class SignUpsController < ApplicationController
 
 	def create
-		s = SignUp.new
-		s.name = params[:name]
-		s.email = params[:email]
+		new_name = params[:sign_up][:name]
+		new_email = params[:sign_up][:email]
+		
+		# s = SignUp.new
+		# s.name = params[:name]
+		# s.email = params[:email]
 		
 		# if I didn't render, it would look for the view create.html.erb
 		# render text: "Created!"
-		if SignUp.find_by(email: s.email) 
+		if SignUp.find_by(email: new_email) 
 			@message = "This e-mail already exists"
-		end
-		
-		if s.name.present? && s.email.present?
-			s.save
+		elsif new_name.present? && new_email.present?
+			SignUp.create({
+				name: new_name,
+				email: new_email
+			})
 			@message = "Saved!"
 		else 
 			@message = "Oops, you should add valid data!"
